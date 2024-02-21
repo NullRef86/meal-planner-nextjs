67c9efa4808f254c6977442cd571eb63a2d299bd
Adam Smithies <adamsmithies86@hotmail.com>
2024-02-21 18:25:42 +0000

    - Fixed select for iOS, maybe...

--- src/app/_components/Select.tsx
+++ src/app/_components/Select.tsx
@@ -1,3 +1,5 @@
+import { faUpDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PropsWithChildren } from "react";
 
 interface IProps extends React.SelectHTMLAttributes<HTMLSelectElement>, PropsWithChildren {
@@ -5,8 +7,10 @@ interface IProps extends React.SelectHTMLAttributes<HTMLSelectElement>, PropsWit
 
 export const Select = ({ children, ...props }: IProps) => {
     return (
-        <select
-            className={`
+        <div className="grid">
+            <select
+                className={`
+                appearance-none row-start-1 col-start-1 
                 bg-gray-50 border
                 border-gray-300
                 text-gray-900
@@ -24,9 +28,14 @@ export const Select = ({ children, ...props }: IProps) => {
                 dark:focus:ring-blue-500
                 dark:focus:border-blue-500
             `}
-            {...props}
-        >
-            {children}
-        </select>
-    )
+                {...props}
+            >
+                {children}
+            </select>
+
+            <svg className="pointer-events-none z-10 right-1 relative col-start-1 row-start-1 h-4 w-4 self-center justify-self-end forced-colors:hidden" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"></path>
+            </svg>
+        </div>
+    );
 }
