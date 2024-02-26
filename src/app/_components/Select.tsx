2989ffdef8947f84413f3306342d3ad5fd340a02
Adam Smithies <adamsmithies86@hotmail.com>
2024-02-26 21:52:42 +0000

    - Swapped out add ingredient select for react-select component.

--- src/app/_components/Select.tsx
+++ src/app/_components/Select.tsx
@@ -10,23 +10,26 @@ export const Select = ({ children, ...props }: IProps) => {
         <div className="grid">
             <select
                 className={`
-                appearance-none row-start-1 col-start-1 
-                bg-gray-50 border
-                border-gray-300
-                text-gray-900
-                text-sm
-                rounded-lg
-                focus:ring-blue-500
-                focus:border-blue-500
-                block
-                w-full
-                p-2.5
-                dark:bg-gray-700
-                dark:border-gray-600
-                dark:placeholder-gray-400
-                dark:text-white
-                dark:focus:ring-blue-500
-                dark:focus:border-blue-500
+                    appearance-none
+                    row-start-1
+                    col-start-1 
+                    bg-gray-50 
+                    border
+                    border-gray-300
+                    text-gray-900
+                    text-sm
+                    rounded-lg
+                    focus:ring-blue-500
+                    focus:border-blue-500
+                    block
+                    w-full
+                    p-2.5
+                    dark:bg-gray-700
+                    dark:border-gray-600
+                    dark:placeholder-gray-400
+                    dark:text-white
+                    dark:focus:ring-blue-500
+                    dark:focus:border-blue-500
             `}
                 {...props}
             >
--- src/app/meals/_components/IncludeIngredientForm.tsx
+++ src/app/meals/_components/IncludeIngredientForm.tsx
@@ -3,16 +3,16 @@ import "core-js/features/array/to-sorted";
 // --------------------------------------------------------------------
 
 import Input from "@/app/_components/Input";
-import { Select } from "@/app/_components/Select";
 import { Button } from "@/app/_components/client-components/Button";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Ingredient } from "@prisma/client";
-import { useContext, useState } from "react";
-import { EMPTY_CATEGORY_VALUE, default as IngredientForm } from "@/app/ingredients/_components/Form";
+import { useContext, useMemo, useState } from "react";
+import { default as IngredientForm } from "@/app/ingredients/_components/Form";
 import { addIngredient, getIngredients } from "@/app/ingredients/actions";
 import { ModalContext } from "@/app/_components/Main";
 import { compareNullableStrings, groupBy } from "@/utils";
+import ReactSelect, { SingleValue } from "react-select";
 
 interface IProps {
     initialIngredientSelectList: Ingredient[];
@@ -40,15 +40,69 @@ const IncludeIngredientForm = ({
 
     const NEW_INGREDIENT_OPTION_KEY = '--New--';
 
+    const groupedOptions = useMemo(() => {
+        return [
+            {
+                label: 'Actions',
+                options: [
+                    {
+                        value: NEW_INGREDIENT_OPTION_KEY,
+                        label: 'Add New Ingredient',
+                    }
+                ]
+            },
+            ...groupBy(ingredientsSelectList, (ingredient) => ingredient.category)
+                .sort(([a], [b]) => compareNullableStrings(a, b))
+                .map(([category, ingredients]) => {
+                    const options = ingredients
+                        .sort((a, b) => a.name.localeCompare(b.name))
+                        .map((ingredient) => (
+                            {
+                                value: ingredient.id,
+                                label: <span>{ingredient.name}{ingredient.units ? <span className="italic dark:text-gray-400 text-xs"> {ingredient.units}</span> : <></>}</span>,
+                            }
+                        ));
+
+                    return (
+                        category
+                            ? ({
+                                label: category,
+                                options
+                            })
+                            : ({
+                                label: 'Uncategorised',
+                                options
+                            })
+                    );
+                })
+        ];
+    }, [ingredientsSelectList]);
+
     return (
         <>
             <div className="flex gap-4">
+
                 <div className="w-1/2">
                     <label>Ingredient:</label>
-                    <Select
-                        name="ingredient"
-                        onChange={(e) => {
-                            if (e.target.value === NEW_INGREDIENT_OPTION_KEY) {
+                    <ReactSelect
+                        className="react-select-container"
+                        classNamePrefix="react-select"
+                        instanceId={'ingredient'}
+                        options={groupedOptions}
+                        value={
+                            groupedOptions.find(option => option.label === formData.ingredient?.category)
+                                ?.options.find(option => option.value === formData.ingredient?.id)
+                            ?? null
+                        }
+                        onChange={(newValue: SingleValue<{
+                            value: number;
+                            label: JSX.Element;
+                        } | {
+                            value: string;
+                            label: string;
+                        }
+                        >) => {
+                            if (newValue?.value === NEW_INGREDIENT_OPTION_KEY) {
                                 modalContext.show({
                                     title: 'Add New Ingredient',
                                     content: (
@@ -74,38 +128,10 @@ const IncludeIngredientForm = ({
 
                             setFormData({
                                 ...formData,
-                                ingredient: ingredientsSelectList?.find((ingredient) => ingredient.id === Number(e.target.value))
+                                ingredient: ingredientsSelectList?.find((ingredient) => ingredient.id === Number(newValue?.value))
                             });
                         }}
-                        value={formData.ingredient?.id ?? ''}
-                    >
-                        <option value="">
-                            Select...
-                        </option>
-                        <option value={NEW_INGREDIENT_OPTION_KEY}>
-                            Add New Ingredient
-                        </option>
-                        {
-                            groupBy(ingredientsSelectList, (ingredient) => ingredient.category)
-                                .sort(([a], [b]) => compareNullableStrings(a, b))
-                                .map(([category, ingredients]) => (
-                                    <optgroup key={category} label={category ?? EMPTY_CATEGORY_VALUE}>
-                                        {
-                                            ingredients
-                                                .sort((a, b) => a.name.localeCompare(b.name))
-                                                .map((ingredient) => (
-                                                    <option
-                                                        key={ingredient.id}
-                                                        value={ingredient.id}
-                                                    >
-                                                        {ingredient.name}{ingredient.units ? ` (${ingredient.units})` : ''}
-                                                    </option>
-                                                ))
-                                        }
-                                    </optgroup>
-                                ))
-                        }
-                    </Select>
+                    />
                 </div>
 
                 <div className="w-1/2">
